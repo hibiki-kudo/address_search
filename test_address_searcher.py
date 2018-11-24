0a56b384cc4163fca79a5e289bdcd5ac3166b66d
hibikikkk <kyou9712@gmail.com>
2018-11-24 17:10:30 +0900

    東京都練馬区豊玉南と岩手県八幡平市大更の郵便番号から変換することができるようにした。
    テストで確認した。

--- test_address_searcher.py
+++ test_address_searcher.py
@@ -1,9 +1,15 @@
 import unittest
+import requests
 
 
 class AddressSearcher:
     def search(self, postal_code):
-        return "岩手県八幡平市大更"
+        url = "http://zipcloud.ibsnet.co.jp/api/search?zipcode=" + postal_code
+        response = requests.get(url).json()["results"][0]
+        都道府県 = response['address1']
+        市町村 = response['address2']
+        町域 = response['address3']
+        return f"{都道府県}{市町村}{町域}"
 
 
 class TestAddressSearcher(unittest.TestCase):
