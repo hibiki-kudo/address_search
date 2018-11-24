7875b74075ad0086360ee7387aeae14c8d3c3710
hibikikkk <kyou9712@gmail.com>
2018-11-24 17:40:27 +0900

    address_searcherクラスのsearchメソッドで共通URLの部分をインスタンス変数にした

--- address_searcher.py
+++ address_searcher.py
@@ -2,8 +2,11 @@ import requests
 
 
 class AddressSearcher:
+    def __init__(self):
+        self.base_url = "http://zipcloud.ibsnet.co.jp/api/search?"
+
     def search(self, postal_code):
-        url = "http://zipcloud.ibsnet.co.jp/api/search?zipcode=" + postal_code
+        url = self.base_url + "zipcode=" + postal_code
         response = requests.get(url).json()["results"][0]
         都道府県 = response['address1']
         市町村 = response['address2']
