987aed59c7650f2a6b83a830cef17f8ac87a6232
hibikikkk <kyou9712@gmail.com>
2018-11-24 15:49:39 +0900

    jsonについて理解することができ、かつrequestsライブラリでテキストhtmlをjson形式に変換できるようになった。

--- request_sample01.py
+++ request_sample01.py
@@ -1,4 +1,10 @@
 import requests
 
+
+def requests_address(address: str):
+    return requests.get(url="http://zipcloud.ibsnet.co.jp/api/search?zipcode=" + address)
+
 if __name__ == "__main__":
-    print(requests)
+    address = requests_address("0287111").json()
+
+    print(address["results"][0]["address1"] + address["results"][0]["address2"] + address["results"][0]["address3"])
